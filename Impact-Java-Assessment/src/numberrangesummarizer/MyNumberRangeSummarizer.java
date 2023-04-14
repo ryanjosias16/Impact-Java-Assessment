9cc271504aaf06d66418188390db3da86f34faac
ryanjosias16 <130476784+ryanjosias16@users.noreply.github.com>
2023-04-15 01:06:58 +0200

    Added Summarizer files from local workstation

---
+++ Impact-Java-Assessment/src/numberrangesummarizer/MyNumberRangeSummarizer.java
@@ -0,0 +1,89 @@
+package numberrangesummarizer;
+
+import java.util.*;
+import java.util.stream.Collectors;
+
+public class MyNumberRangeSummarizer implements NumberRangeSummarizer {
+
+    public static void main(String[] args) {
+        MyNumberRangeSummarizer summarizer = new MyNumberRangeSummarizer();
+        System.out.println("*** Number Range Summarizer ***");
+        System.out.println("Please enter a sequence of numbers separated by comma:");
+        Scanner scan = new Scanner(System.in);
+        String input = scan.nextLine();
+        Collection<Integer> collection = summarizer.collect(input);
+        String summary = summarizer.summarizeCollection(collection);
+        System.out.println(summary);
+    }
+
+    @Override
+    public Collection<Integer> collect(String input) {
+        
+        //remove all white space
+        input = input.replaceAll("\\s+", "");
+
+        //Throw an exception if input string is empty or null
+        if(input == null || input.trim().isEmpty()) {
+            throw new IllegalArgumentException("String input cannot be empty or null");
+        }
+
+        String[] stringArray = input.split(",");
+
+        //Throw an exception if the array contains any invalid characters
+        for (String str : stringArray) {
+            if (!str.matches("-?\\d+")) {
+                throw new IllegalArgumentException("Invalid input. Input must be in the form of numbers separated by comma.");
+            }
+        }
+
+        List<Integer> numbers = new ArrayList<>();
+
+        //Loop through the array and add it to an Array list object
+        for (int i = 0; i < stringArray.length; i++) {
+            numbers.add(Integer.parseInt(stringArray[i]));
+        }
+
+        Collections.sort(numbers);
+
+        //Returns a sorted ArrayList
+        return numbers; 
+    }
+
+    @Override
+    public String summarizeCollection(Collection<Integer> input) {
+        
+        //Use hashset to retrieve only unique values from input
+        HashSet<Integer> set = new HashSet<>(input);
+        
+        List<Integer> numbers = new ArrayList<>();
+        numbers.addAll(set);
+        Collections.sort(numbers);
+        List<String> ranges = new ArrayList<>();
+        int rangeStart = numbers.get(0);
+
+        //Loop through each item in the ArrayList
+        for (int i = 1; i <= numbers.size(); i++) {
+
+            //If i is the last element OR i is not sequential to the previous element's value, enter the if statement , else do nothing and continue through the for loop
+            if (i == numbers.size() || numbers.get(i) != numbers.get(i - 1) + 1) {
+               
+                //If i-1 was not part of a sequence, add i-1 to the ranges List
+                if (rangeStart == numbers.get(i - 1)) {
+                    ranges.add(String.valueOf(rangeStart));
+                    
+                //Else if i-1 was part of a sequence, add the sequence range to the ranges List
+                }
+                else {
+                    ranges.add(rangeStart + "-" + numbers.get(i - 1));
+                }
+                
+                //If i is not the last element, reset the range start
+                if (i < numbers.size()) {
+                    rangeStart = numbers.get(i);
+                }
+            }
+        }
+        String output = ranges.stream().collect(Collectors.joining(", "));
+        return output;
+    }
+}
---
+++ Impact-Java-Assessment/src/test/MyNumberRangeSummarizerTest.java
@@ -0,0 +1,116 @@
+package test;
+
+import numberrangesummarizer.MyNumberRangeSummarizer;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MyNumberRangeSummarizerTest {
+
+    @Test
+    void testCollectionSorting() {
+        MyNumberRangeSummarizer summarizer = new MyNumberRangeSummarizer();
+        List<Integer> expectedResult = Arrays.asList(5, 10, 21, 22, 23, 60, 71, 72, 73);
+        assertEquals(expectedResult, summarizer.collect("10,22,21,23,5,60,73,71,72"));
+    }
+
+    @Test
+    void testCollectionWithSingleNumber() {
+        MyNumberRangeSummarizer summarizer = new MyNumberRangeSummarizer();
+        List<Integer> expectedResult = Arrays.asList(9);
+        assertEquals(expectedResult, summarizer.collect("9"));
+    }
+
+    @Test
+    void testSummaryWithSingleNumber() {
+        MyNumberRangeSummarizer summarizer = new MyNumberRangeSummarizer();
+        String expectedResult = "9";
+        List<Integer> input = Arrays.asList(9);
+        assertEquals(expectedResult, summarizer.summarizeCollection(input));
+    }
+
+    @Test
+    void testCollectionWithNoInput() {
+        MyNumberRangeSummarizer summarizer = new MyNumberRangeSummarizer();
+        assertThrows(IllegalArgumentException.class, () -> {
+            summarizer.collect("");
+        });
+    }
+
+    @Test
+    void testCollectionWithInvalidCharacters() {
+        MyNumberRangeSummarizer summarizer = new MyNumberRangeSummarizer();
+        assertThrows(IllegalArgumentException.class, () -> {
+            summarizer.collect("1,2,3,4,a,b,c");
+        });
+    }
+
+    @Test
+    void testCollectionWithInvalidInput() {
+        MyNumberRangeSummarizer summarizer = new MyNumberRangeSummarizer();
+        assertThrows(IllegalArgumentException.class, () -> {
+            summarizer.collect("qwertyuiop[];'.,");
+        });
+    }
+
+    @Test
+    void testSummaryMultipleGroupingsOfNumbers() {
+        MyNumberRangeSummarizer summarizer = new MyNumberRangeSummarizer();
+        String expectedResult = "1-3, 5-7, 10-12, 15, 21-24";
+        List<Integer> input = Arrays.asList(1, 2, 3, 5, 6, 7, 10, 11, 12, 15, 21, 22, 23, 24);
+        assertEquals(expectedResult, summarizer.summarizeCollection(input));
+    }
+
+    @Test
+    void testSummaryTwoConsecutiveNumberGroupings() {
+        MyNumberRangeSummarizer summarizer = new MyNumberRangeSummarizer();
+        String expectedResult = "1-2, 4-5, 7, 10-11, 14-15, 20, 22-23, 29";
+        List<Integer> input = Arrays.asList(1, 2, 4, 5, 7, 10, 11, 14, 15, 20, 22, 23, 29);
+        assertEquals(expectedResult, summarizer.summarizeCollection(input));
+    }
+
+
+    @Test
+    void testCollectionWithNegativeNumbers() {
+        MyNumberRangeSummarizer summarizer = new MyNumberRangeSummarizer();
+        List<Integer> expectedResult = Arrays.asList(-3,-2,-1,1,2,3);
+        assertEquals(expectedResult, summarizer.collect("-3,-2,-1,1,2,3"));
+    }
+
+    @Test
+    void testSummaryWithNegativeNumbers() {
+        MyNumberRangeSummarizer summarizer = new MyNumberRangeSummarizer();
+        String expectedResult = "-3--1, 1-3";
+        List<Integer> input = Arrays.asList(-3, -2, -1, 1, 2, 3);
+        assertEquals(expectedResult, summarizer.summarizeCollection(input));
+    }
+
+    @Test
+    void testCollectionWithAdditionalWhitespace() {
+        MyNumberRangeSummarizer summarizer = new MyNumberRangeSummarizer();
+        List<Integer> expectedResult = Arrays.asList(-3,-2,-1,1,2,3);
+        assertEquals(expectedResult, summarizer.collect("-3,-2,-1 ,1,2, 3"));
+    }
+
+    @Test
+    void testCollectionWithDuplicateNumbers() {
+        MyNumberRangeSummarizer summarizer = new MyNumberRangeSummarizer();
+        List<Integer> expectedResult = Arrays.asList(1, 2, 2, 3, 3, 3, 4, 5);
+        assertEquals(expectedResult, summarizer.collect("1,2,2,3,3,3,4,5"));
+    }
+
+    @Test
+    void testSummaryWithDuplicateNumbers() {
+        MyNumberRangeSummarizer summarizer = new MyNumberRangeSummarizer();
+        String expectedResult = "1-5";
+        List<Integer> input = Arrays.asList(1, 2, 2, 3, 3, 3, 4, 5);
+        assertEquals(expectedResult, summarizer.summarizeCollection(input));
+
+    }
+
+}
